24658005872e1c6ef3d2b473033d02e201797fd6
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2023-02-05 17:08:56 +0100

    add auth

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,31 +2,37 @@ const express = require('express');
 const routerContacts = express.Router();
 const ctrlTasks = require("../../controllers/contacts.controller.js");
 const { tryCatchWrapper} = require("../../models/helpers/index.js")
-const { validateBody, isValidId } = require("../../middelwares/index")
+const { validateBody, isValidId, auth } = require("../../middelwares/index")
 const { schemas } = require("../../models/contact");
 
 
 routerContacts.get('/',
+    auth,
     tryCatchWrapper(ctrlTasks.getContactsService));
 
 routerContacts.get('/:id',
+    auth,
     isValidId,
     tryCatchWrapper(ctrlTasks.getContactByIdService));
 
 routerContacts.post('/',
+    auth,
     validateBody(schemas.addContactSchema),
     tryCatchWrapper(ctrlTasks.creacteContactService));
 
 routerContacts.delete('/:id',
+    auth,
     isValidId,
     tryCatchWrapper(ctrlTasks.deleteContactService));
  
 routerContacts.put('/:id',
+    auth,
     isValidId,
     validateBody(schemas.addContactSchema),
     tryCatchWrapper(ctrlTasks.updateContactService));
 
 routerContacts.patch('/:id/favorite',
+    auth,
     isValidId,
     validateBody(schemas.updateFavoriteSchema),
     tryCatchWrapper(ctrlTasks.updateFavoriteService))
