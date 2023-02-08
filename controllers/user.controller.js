a00af75f74fedc1fc9be5a7c37a6ffd93f365ebe
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2023-02-08 12:39:22 +0100

    add upload avatar

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,7 @@
 const HttpError = require('../models/helpers/HttpError');
 const { User } = require('../models/user');
+const path = require('path');
+const fs = require('fs/promises');
 
 async function current(req, res, next) {
     const { _id } = req.user;
@@ -33,32 +35,45 @@ async function logout(req, res, next) {
 }
 
 async function updateSubscription(req, res, next) {
-    // const { _id } = req.user;
-    // const { subscription } = req.body;
-    // const user = await User.findById(_id);
-
-    //  if (!user || !user.token)
-    //     return next(new HttpError(401, "Not authorized"));
-    // const updatedUser = await User.findByIdAndUpdate(
-    //     _id,
-    //     { subscription: subscription },
-    //     { new: true }
-    // );
-
-    // res.json({
-    //     email: user.email,
-    //     subscription: updatedUser.subscription,
-    // });
-
     return res.status(200).json({
         ok: true,
     });
+}
+
+const uploadAvatar = async (req, res, next) => {
+  // req.file
+    console.log('req.file', req.file);
+    const { filename } = req.file;
+    const tmpPath = path.resolve(__dirname, '../tmp', filename);
+    const publicPath = path.resolve(__dirname, '../public/avatars', filename);
+
+    try { 
+        await fs.rename(tmpPath, publicPath);
+    } catch (error) {
+        await fs.unlink(tmpPath);
+        throw error;
+    }
+
+    const { _id } = req.user;
+    
+    const imagePath = `/public/avatars/${filename}`;
+    const userAvatar = await User.findByIdAndUpdate(_id,
+        { avatarURL: publicPath, },
+        { new: true }
+    );
+
+  return res.json({
+      data: {
+          avatarURL: imagePath,
+    }
+  });
 }
 
 module.exports = {
     current,
     logout,
     updateSubscription,
+    uploadAvatar,
 }
 
 // current, logout, updateSubscription
