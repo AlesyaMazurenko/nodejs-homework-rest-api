7c6c3d07b0f587265d8400f79d5812ed2528940b
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2023-02-10 01:10:04 +0100

    Update user.js

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const userRouter = express.Router();
-const {current, logout, updateSubscription, uploadAvatar} = require("../../controllers/user.controller.js");
-const { tryCatchWrapper} = require("../../models/helpers/index.js")
-const { auth, upload } = require("../../middelwares/index")
+const {current, logout, updateSubscription, uploadAvatar, verifyEmail, resendVerifyEmail} = require("../../controllers/user.controller.js");
+const { tryCatchWrapper, validateBody} = require("../../models/helpers/index.js")
+const { auth, upload } = require("../../middelwares/index");
+const { verifyEmailSchema } = require('../../middelwares/shema');
 
 userRouter.get('/current',
     tryCatchWrapper(auth),
@@ -20,5 +21,13 @@ userRouter.patch('/avatars',
     tryCatchWrapper(auth),
     upload.single('image'),
     tryCatchWrapper(uploadAvatar));
+
+userRouter.get('/verify/:verificationToken',
+    tryCatchWrapper(verifyEmail));
+
+userRouter.post('/verify',
+    validateBody(verifyEmailSchema),
+    tryCatchWrapper(resendVerifyEmail),
+);
 
 module.exports = { userRouter };
